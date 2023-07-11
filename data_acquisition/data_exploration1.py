a25ee539b4f15c2c5373d89b4a0ad53c5bebf4b2
= <connor.park2573@gmail.com>
2023-07-11 18:07:11 -0400

    seting up new base

--- data_acquisition/data_exploration1.py
+++
@@ -1,4 +0,0 @@
-import pandas
-
-df = pandas.read_csv('./data/AAPL.csv')
-print(df.head())
--- data_acquisition/quandl_data.py
+++
@@ -1,6 +0,0 @@
-import quandl
-
-quandl.read_key()
-ticker = 'NVDA'
-data = quandl.get('WIKI/' + ticker)
-data.to_csv('./data/' + ticker + '.csv')
